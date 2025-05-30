2f45d4a9ef258821a61ed69045c20bdc047a8cc8
sasha <sashamatvienko499@gmail.com>
2025-05-30 18:17:00 +0300

    1

--- src/db.py
+++ src/db.py
@@ -7,17 +7,65 @@ Base = declarative_base()
 #Сотрудники: ФИО, дата рождения, паспортные данные,
 #банковские реквизиты, наличие семьи, состояние здоровья.
 
-class Employee(Base):
-    __tablename__ = 'employees'
+class Product(Base):
+    __tablename__ = 'products'
     id = Column(Integer, primary_key=True)
-    full_name = Column(String(100), nullable=False)
-    birth_date = Column(DateTime)
-    passport_data = Column(String(50))
-    bank_details = Column(String(100))
-    has_family = Column(Boolean)
-    health_status = Column(String(200))
-    contact_phone = Column(String(20))
+    article = Column(String(50), unique=True)
+    product_type = Column(String(50))
+    name = Column(String(100), nullable=False)
+    description = Column(String(500))
+    image_path = Column(String(200))
+    min_partner_price = Column(Float)
+    package_length = Column(Float)
+    package_width = Column(Float)
+    package_height = Column(Float)
+    net_weight = Column(Float)
+    gross_weight = Column(Float)
+    quality_certificate_path = Column(String(200))
+    standard_number = Column(String(50))
+    production_time = Column(Integer)  # в часах
+    cost_price = Column(Float)
+    workshop_number = Column(Integer)
+    required_workers = Column(Integer)
+
+
+
+class Partners(Base):
+    __tablename__ = 'partners'
+    id = Column(Integer, primary_key=True)
+    partner_type = Column(String(50))  # розница, опт, интернет-магазин и т.д.
+    company_name = Column(String(100), nullable=False)
+    legal_address = Column(String(200))
+    inn = Column(String(12), unique=True)
+    director_name = Column(String(100))
+    phone = Column(String(20))
     email = Column(String(50))
+    logo_path = Column(String(200))
+    rating = Column(Integer)
+
+
+
+class Type_marerial(Base):
+    __tablename__ = 'Type_marerial'
+    id = Column(Integer, primary_key=True)
+    Type_marerial = Column(String(50))  # розница, опт, интернет-магазин и т.д.
+    procent_brack = Column(Float(10), nullable=False)
+
+
+
+class Prodaga(Base):
+    __tablename__ = 'prodaga'
+    id = Column(Integer, primary_key=True)
+    product = Column(String(50))  # розница, опт, интернет-магазин и т.д.
+    partner_name = Column(String(100), nullable=False)
+    kol_tovara = Column(Integer)
+    inn = Column(String(12), unique=True)
+    director_name = Column(String(100))
+    phone = Column(String(20))
+    email = Column(String(50))
+    logo_path = Column(String(200))
+    rating = Column(Integer)
+
 
 # Создание базы данных
 engine = create_engine('sqlite:///master_pol.db')
